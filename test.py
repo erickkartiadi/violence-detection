1a54212a3074bd124f81f63db7cc30e58694ce08
erickkartiadi <erickcartiady@gmail.com>
2024-10-09 18:31:59 +0700

    upload

---
+++ test.py
@@ -0,0 +1,5 @@
+from google.colab import drive
+drive.mount('/content/drive')
+DIR_PATH = "/content/drive/MyDrive/RWF-2000"
+TRAIN_DIR = os.path.join(DIR_PATH, "train")
+TEST_DIR = os.path.join(DIR_PATH, "val")

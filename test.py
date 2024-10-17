5d3a66f7e0148c8df57cc05016718781d464b717
erickkartiadi <erickcartiady@gmail.com>
2024-10-17 17:59:37 +0700

    tidy up

--- test.py
+++
@@ -1,5 +0,0 @@
-from google.colab import drive
-drive.mount('/content/drive')
-DIR_PATH = "/content/drive/MyDrive/RWF-2000"
-TRAIN_DIR = os.path.join(DIR_PATH, "train")
-TEST_DIR = os.path.join(DIR_PATH, "val")
